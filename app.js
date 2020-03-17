e6095a7da320ef9dcda907cea53c7b04196266f7
bieleckj <jakub.bielecki@contractors.roche.com>
2020-03-17 23:52:20 +0100

    auth

--- app.js
+++ app.js
@@ -1,43 +1,76 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const dishRouter = require('./routes/dishRouter');
-const promoRouter = require('./routes/promoRouter');
-const leaderRouter = require('./routes/leaderRouter');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const usersRouter = require("./routes/users");
 
 const app = express();
-const url = 'mongodb://localhost:27017/conFusion';//mongo server url
+const url = "mongodb://localhost:27017/conFusion"; //mongo server url
 const connect = mongoose.connect(url); //connecting to mongo server with mongose
 
-connect.then((db) => {
+connect.then(
+  db => {
     console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  },
+  err => {
+    console.log(err);
+  }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('12345-67890-09876-54321'));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+const auth = (req, res, next) => {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  if(!authHeader) {
+    const err = new Error('You are not authenticated !');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+
+    return next(err);
+  }
+  const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const username = auth[0];
+  const password = auth[1];
+
+  if(username === 'admin' && password === 'password') {
+    next();
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+};
+
+app.use(auth);
+
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
@@ -45,11 +78,11 @@ app.use(function(req, res, next) {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
